847e432734d75599ad2309a5ed6e62222bbb3254
LadaKuzmina <92179198+LadaKuzmina@users.noreply.github.com>
2023-10-06 19:18:46 +0500

    Приложение для терминала

--- src/core/cli-command/helper.command.ts
+++ src/core/cli-command/helper.command.ts
@@ -8,10 +8,10 @@ export default class HelperCommand implements CliCommandInterface {
         console.log(`
         Программа для подготовки данных для REST API сервера.
         Пример: cli.js --<command> [--arguments]\n`,
-            chalk.bold(`\tКоманды:\n`),
-            chalk.yellow(`\t--version:                   `), chalk.bgMagenta(`# выводит номер версии\n`),
-            chalk.green(`\t--help:                      `), chalk.bgMagenta(`# печатает этот текст\n`),
-            chalk.red(`\t--import <path>:             `), chalk.bgMagenta(`# импортирует данные из TSV\n`),
-            chalk.blue(`\t--generate <n> <path> <url>  `), chalk.bgMagenta(`# генерирует произвольное количество тестовых данных`));
+            chalk.bold('\tКоманды:\n'),
+            chalk.yellow('\t--version:                   '), chalk.bgMagenta('# выводит номер версии\n'),
+            chalk.green('\t--help:                      '), chalk.bgMagenta('# печатает этот текст\n'),
+            chalk.red('\t--import <path>:             '), chalk.bgMagenta('# импортирует данные из TSV\n'),
+            chalk.blue('\t--generate <n> <path> <url>  '), chalk.bgMagenta('# генерирует произвольное количество тестовых данных'));
     }
 }
