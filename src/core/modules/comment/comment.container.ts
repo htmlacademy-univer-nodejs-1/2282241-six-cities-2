b63ca2327ee7938522923fa93c1acbb01291d3dd
LadaKuzmina <92179198+LadaKuzmina@users.noreply.github.com>
2023-11-09 01:06:34 +0500

    Завершаем проектировать базу данных

---
+++ src/core/modules/comment/comment.container.ts
@@ -0,0 +1,13 @@
+import {Container} from "inversify";
+import {CommentServiceInterface} from "./comment-service.interface";
+import {AppComponent} from "../../../types/app-component.enum";
+import {types} from "@typegoose/typegoose";
+import {CommentEntity, CommentModel} from "./comment.entity";
+import {CommentService} from "./comment.service";
+
+export function CreateCommentContainer() {
+    const commentContainer = new Container();
+    commentContainer.bind<CommentServiceInterface>(AppComponent.CommentServiceInterface).to(CommentService).inSingletonScope();
+    commentContainer.bind<types.ModelType<CommentEntity>>(AppComponent.CommentModel).toConstantValue(CommentModel);
+    return commentContainer;
+}
