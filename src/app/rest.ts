67055890c6fd48e9d6714162a63a171a1a571157
LadaKuzmina <92179198+LadaKuzmina@users.noreply.github.com>
2023-11-10 09:47:54 +0500

    Сервер наносит ответный удар

--- src/app/rest.ts
+++ src/app/rest.ts
@@ -5,12 +5,16 @@ import {inject, injectable} from 'inversify';
 import {AppComponent} from '../types/app-component.enum.js';
 import {DatabaseClientInterface} from '../core/database-client/database-client.interface.js';
 import {getMongoURI} from '../core/helpers/db.js';
+import express, {Express} from 'express';
 
 @injectable()
 export default class Application {
+  private server: Express;
+
   constructor(@inject(AppComponent.LoggerInterface) private readonly logger: LoggerInterface,
               @inject(AppComponent.ConfigInterface) private readonly config: ConfigInterface<RestSchema>,
               @inject(AppComponent.DatabaseClientInterface) private readonly databaseClient: DatabaseClientInterface) {
+    this.server = express();
   }
 
   private async _initDb() {
@@ -25,11 +29,18 @@ export default class Application {
     return this.databaseClient.connect(mongoUri);
   }
 
+  private async _initServer() {
+    const port = this.config.get('PORT');
+    this.server.listen(port);
+  }
+
   public async init() {
     this.logger.info('Application init');
-    this.logger.info(`Get value from env $PORT: ${this.config.get('PORT')}`);
     this.logger.info('Init database');
     await this._initDb();
     this.logger.info('Init database completed');
+    this.logger.info('Try to init server');
+    await this._initServer();
+    this.logger.info(`Server started on http://localhost:${this.config.get('PORT')}`);
   }
 }
