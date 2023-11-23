b9b5d7a398129eebc41959a83460517ddb1c1ff4
LadaKuzmina <92179198+LadaKuzmina@users.noreply.github.com>
2023-11-24 00:55:44 +0500

    Дорожные работы

--- src/core/modules/offer/offer-service.interface.ts
+++ src/core/modules/offer/offer-service.interface.ts
@@ -2,8 +2,9 @@ import {DocumentType} from '@typegoose/typegoose';
 import {OfferEntity} from './offer.entity.js';
 import CreateOfferDto from './dto/create-offer.dto.js';
 import UpdateOfferDto from './dto/update-offer-dto.js';
+import {DocumentExists} from '../../../types/document-exists.interface';
 
-export interface OfferServiceInterface {
+export interface OfferServiceInterface extends DocumentExists {
     create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
 
     findById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
@@ -19,8 +20,6 @@ export interface OfferServiceInterface {
     incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null>;
 
     getPremium(): Promise<DocumentType<OfferEntity>[]>;
-
-    // getFavorite(): Promise<DocumentType<OfferEntity>[]>;
 
     calculationRating(rating: number, newRating: number, countRating:number, offerId:string): Promise<void>;
 
